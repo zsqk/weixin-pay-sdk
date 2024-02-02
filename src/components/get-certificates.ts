fc5ab374817e2c583f5960eacc75048187fd950a
iugo <iugogogo@gmail.com>
2024-02-02 17:25:45 +0800

    feat: 新增 获取平台证书 功能

---
+++ src/components/get-certificates.ts
@@ -0,0 +1,47 @@
+/**
+ * [业务类型] 微信支付 平台证书
+ * @link <https://pay.weixin.qq.com/docs/merchant/apis/platform-certificate/api-v3-get-certificates/get.html>
+ */
+export type Certificate = {
+  /**
+   * 【证书序列号】 平台证书的主键，唯一定义此资源的标识
+   */
+  serial_no: string;
+  /**
+   * 【证书启用时间】 启用证书的时间，时间格式为RFC3339。每个平台证书的启用时间是固定的。
+   */
+  effective_time?: string;
+  /**
+   * 【证书弃用时间】 弃用证书的时间，时间格式为RFC3339。更换平台证书前，会提前24小时修改老证书的弃用时间，接口返回新老两个平台证书。更换完成后，接口会返回最新的平台证书。
+   */
+  expire_time?: string;
+  /**
+   * 【证书信息】 证书内容
+   */
+  encrypt_certificate: {
+    /**
+     * 【加密证书的算法】 对开启结果数据进行加密的加密算法，目前只支持AEAD_AES_256_GCM。
+     */
+    algorithm: string;
+    /**
+     * 【加密证书的附加数据】 加密证书的附加数据，固定为“certificate"。
+     */
+    associated_data?: string;
+    /**
+     * 【加密后的证书内容】 使用API KEY和上述参数，可以解密出平台证书的明文。证书明文为PEM格式。（注意：更换证书时会出现PEM格式中的证书失效时间与接口返回的证书弃用时间不一致的情况）
+     */
+    ciphertext: string;
+    /**
+     * 【加密证书的随机串】 对应到加密算法中的IV。
+     */
+    nonce: string;
+  };
+};
+
+/**
+ * 断言参数为微信支付平台证书
+ * @param v
+ */
+export function assertCertificate(v: unknown): asserts v is Certificate {
+  // TODO: check
+}
--- src/wxpay.ts
+++ src/wxpay.ts
@@ -1,4 +1,12 @@
 import { genAuth } from './common/gen-auth.ts';
+import {
+  assertArray,
+  assertUnknownObject,
+} from 'https://deno.land/x/somefn@v0.27.1/ts/object.ts';
+import {
+  assertCertificate,
+  Certificate,
+} from './components/get-certificates.ts';
 
 export class WxpaySDK {
   /** 商户号 */
@@ -13,18 +21,24 @@ export class WxpaySDK {
   /** RSA 密钥, 保密 */
   private readonly keyString: string;
 
+  isDebug = false;
+
   constructor(
-    { mchid, rsaSN, endpoint, keyString }: {
+    { mchid, rsaSN, endpoint, keyString, isDebug }: {
       mchid: string;
       rsaSN: string;
       keyString: string;
       endpoint?: string;
+      isDebug?: boolean;
     },
   ) {
     this.mchid = mchid;
     this.rsaSN = rsaSN;
     this.keyString = keyString;
     this.endpoint = endpoint ?? 'https://api.mch.weixin.qq.com';
+    if (isDebug !== undefined) {
+      this.isDebug = isDebug;
+    }
   }
 
   private genAuth(opt: {
@@ -39,27 +53,49 @@ export class WxpaySDK {
     });
   }
 
+  private log(...data: unknown[]): void {
+    if (this.isDebug) {
+      console.log(...data);
+    }
+  }
+
   async request({
     method,
     path,
     body,
+    headers,
   }: {
     method: 'POST' | 'GET';
     path: string;
-    body: unknown;
+    body?: unknown;
+    headers?: Record<string, string>;
   }): Promise<unknown> {
     const bodyStr = JSON.stringify(body);
-    const res = await fetch(`${this.endpoint}${path}`, {
-      method,
-      headers: {
-        Authorization: await this.genAuth({ method, path, body: bodyStr }),
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'Accept-Language': 'zh-CN',
-        'User-Agent': 'zsqk-z1 0.0.1',
-      },
-      body: bodyStr,
-    });
+    const ua = 'zsqk-z1 0.0.1';
+    const requestInit: RequestInit = body
+      ? {
+        method,
+        headers: {
+          'User-Agent': ua,
+          ...headers,
+          Authorization: await this.genAuth({ method, path, body: bodyStr }),
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'Accept-Language': 'zh-CN',
+        },
+        body: bodyStr,
+      }
+      : {
+        method,
+        headers: {
+          'User-Agent': ua,
+          ...headers,
+          Authorization: await this.genAuth({ method, path, body: '' }),
+          Accept: 'application/json',
+          'Accept-Language': 'zh-CN',
+        },
+      };
+    const res = await fetch(`${this.endpoint}${path}`, requestInit);
 
     const text = await res.text();
 
@@ -70,9 +106,28 @@ export class WxpaySDK {
       throw new Error(`${res.status} ${text}`);
     }
 
-    if (res.headers.get('Content-Type') !== 'application/json') {
-      return text;
+    this.log('res.headers', res.headers);
+    if (res.headers.get('Content-Type')?.includes('application/json')) {
+      return JSON.parse(text);
     }
-    return JSON.parse(text);
+    return text;
+  }
+
+  /**
+   * 获取平台证书
+   * 接口的频率限制: 单个商户号1000 次/s
+   * @link <https://pay.weixin.qq.com/docs/merchant/apis/platform-certificate/api-v3-get-certificates/get.html>
+   * @returns
+   */
+  public async getCertificates(): Promise<{ data: Certificate[] }> {
+    const res = await this.request({ method: 'GET', path: '/v3/certificates' });
+    assertUnknownObject(res);
+    assertArray(res.data);
+    return {
+      data: res.data.map((v) => {
+        assertCertificate(v);
+        return v;
+      }),
+    };
   }
 }
